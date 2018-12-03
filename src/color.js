180dd67221cfa34482a162c05ddd1ec6c882580d
Julien Droulez <julien.droulez@free.fr>
2018-12-03 12:48:46 +0100

    parse functions and byName now can  return Color instance or POJO

--- src/color.js
+++ src/color.js
@@ -178,28 +178,28 @@ function Color(v){
 	if(v){
 		if(typeof v == 'string'){
 			if(v.length > 0 && v[0] == "#"){
-				_rgb = Color.parse.hex(v);
-				_a = _rgb.a;
+				_rgb = Color.parse.hex(v, true);
+				_a = _rgb && _rgb.a || 0;
 				delete _rgb.a;
 			}else if(v.length > 4 && v.substring(0, 5) == "hsla"){
-				_hsl = Color.parse.hsla(v);
-				_a = hsl.a;
+				_hsl = Color.parse.hsla(v, true);
+				_a = _hsl && _hsl.a || 0;
 				delete _hsl.a;
 			}else if(v.length > 4 && v.substring(0, 5) == "rgba"){
-				_rgb = Color.parse.rgba(v);
-				_a = _rgb.a;
+				_rgb = Color.parse.rgba(v, true);
+				_a = _rgb && _rgb.a || 0;
 				delete _rgb.a;
 			}else if(v.length > 3 && v.substring(0, 4) == "hsl"){
-				_hsl = Color.parse.hsl(v);
-				_a = hsl.a;
+				_hsl = Color.parse.hsl(v, true);
+				_a = _rgb && _rgb.a || 0;
 				delete _hsl.a;
 			}else if(v.length > 4 && v.substring(0, 4) == "rgb"){
-				_rgb = Color.parse.rgb(v);
-				_a = _rgb.a;
+				_rgb = Color.parse.rgb(v, true);
+				_a = _rgb && _rgb.a || 0;
 				delete _rgb.a;
-			}else if(Color.byName(v, true)){
-				_rgb = Color.parse.hex(Color.byName(v, true));
-				_a = _rgb.a;
+			}else{
+				_rgb = Color.byName(v, true);
+				_a = _rgb && _rgb.a || 0;
 				delete _rgb.a;
 			}
 		}else{
@@ -235,7 +235,6 @@ function Color(v){
 			_rgb = {r:0, g: 0, b:0};
 		}
 	}
-
 }
 
 Color.prototype.equals = function color_equals (other){
@@ -262,11 +261,12 @@ Color.prototype.hexString = function(includeAlpha){
 
 Color.prototype.toString = Color.prototype.rgbString;
 
-Color.byName = function(name, dontInstanciate){
+Color.byName = function(name, asPOJO){
 	var index = {"black":"#000000","silver":"#c0c0c0","gray":"#808080","white":"#ffffff","maroon":"#800000","red":"#ff0000","purple":"#800080","fuchsia":"#ff00ff","green":"#008000","lime":"#00ff00","olive":"#808000","yellow":"#ffff00","navy":"#000080","blue":"#0000ff","teal":"#008080","aqua":"#00ffff","orange":"#ffa500","aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aquamarine":"#7fffd4","azure":"#f0ffff","beige":"#f5f5dc","bisque":"#ffe4c4","blanchedalmond":"#ffebcd","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887","cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff","darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkgrey":"#a9a9a9","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f","darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkslategrey":"#2f4f4f","darkturquoise":"#00ced1","darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dimgrey":"#696969","dodgerblue":"#1e90ff","firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","greenyellow":"#adff2f","grey":"#808080","honeydew":"#f0fff0","hotpink":"#ff69b4","indianred":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c","lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2","lightgray":"#d3d3d3","lightgreen":"#90ee90","lightgrey":"#d3d3d3","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightslategrey":"#778899","lightsteelblue":"#b0c4de","lightyellow":"#ffffe0","limegreen":"#32cd32","linen":"#faf0e6","magenta":"#ff00ff","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370db","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee","mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5","navajowhite":"#ffdead","oldlace":"#fdf5e6","olivedrab":"#6b8e23","orangered":"#ff4500","orchid":"#da70d6","palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#db7093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","rosybrown":"#bc8f8f","royalblue":"#4169e1","saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","slategrey":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4","tan":"#d2b48c","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0","violet":"#ee82ee","wheat":"#f5deb3","whitesmoke":"#f5f5f5","yellowgreen":"#9acd32","rebeccapurple":"#663399"};
 	var hex = index[name];
-	if(dontInstanciate){
-		return hex;
+	if(!hex) return false;
+	if(asPOJO){
+		return Color.parse.hex(hex, true);
 	}
 	return new Color(hex);
 };
@@ -354,32 +354,33 @@ Color.parse = function parseColor(str){
 };
 
 
-Color.parse.hex = function parseHEX(str){
+Color.parse.hex = function parseHEX(str, asPOJO){
 	str = str.trim();
+	var rgb = false;
 	if(str[0] != "#") return false;
 	if(str.length == 4){
-		return {
+		 rgb = {
 			r: parseInt(str.substring(1,2) + str.substring(1,2), 16),
 			g: parseInt(str.substring(2,3) + str.substring(2,3), 16),
 			b: parseInt(str.substring(3)   + str.substring(3),   16),
 			a: 1
 		}
 	}else if(str.length == 7){
-		return {
+		rgb = {
 			r: parseInt(str.substring(1,3), 16),
 			g: parseInt(str.substring(3,5), 16),
 			b: parseInt(str.substring(5),   16),
 			a: 1
 		}
 	}else if(str.length == 5){
-		return {
+		rgb = {
 			r: parseInt(str.substring(1,2) + str.substring(1,2), 16),
 			g: parseInt(str.substring(2,3) + str.substring(2,3), 16),
 			b: parseInt(str.substring(3,4) + str.substring(3,4), 16),
 			a: parseInt(str.substring(4)   + str.substring(4)  , 16)
 		}
 	}else if(str.length == 9){
-		return {
+		rgb = {
 			r: parseInt(str.substring(1,3), 16),
 			g: parseInt(str.substring(3,5), 16),
 			b: parseInt(str.substring(5,7), 16),
@@ -387,63 +388,73 @@ Color.parse.hex = function parseHEX(str){
 		}
 	}
 
-	return false;
+	if(!rgb || asPOJO) return rgb;
+
+	return new Color(rgb);
 };
 
 
-Color.parse.rgb = function parseRGB(str){
+Color.parse.rgb = function parseRGB(str, asPOJO){
 	var re = /^\s*rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)\s*$/;
 	var match = str.match(re);
 	if(!match) return false;
-	return {
+	var rgb =  {
 		r: Math.max(0, Math.min(255, parseInt(match[1]))),
 		g: Math.max(0, Math.min(255, parseInt(match[2]))),
 		b: Math.max(0, Math.min(255, parseInt(match[3]))),
 		a: 1
 	};
+	if(asPOJO) return rgb;
+	return new Color(rgb);
 };
 
-Color.parse.rgba = function parseRGBA(str){
+Color.parse.rgba = function parseRGBA(str, asPOJO){
 	var re = /^\s*rgba\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d?\.?\d+)\s*\)\s*$/;
 	var match = str.match(re);
 	if(!match) return false;
-	return {
+	var rgb = {
 		r: Math.max(0, Math.min(255, parseInt(match[1]))),
 		g: Math.max(0, Math.min(255, parseInt(match[2]))),
 		b: Math.max(0, Math.min(255, parseInt(match[3]))),
 		a: Math.max(0, Math.min(1, 	 parseFloat(match[4])))
 	};
+	if(asPOJO) return rgb;
+	return new Color(rgb);
 };
 
 
 
-Color.parse.hsl = function parseHSL(str){
+Color.parse.hsl = function parseHSL(str, asPOJO){
 	var re = /^\s*hsl\s*\(\s*(\d*\.?\d+)\s*,\s*(\d*\.?\d+)\s*%\s*,\s*(\d*\.?\d+)\s*%\s*\)\s*$/;
 	var match = str.match(re);
 	if(!match) return false;
 	var h = parseFloat(match[1]) % 360;
 	if(h < 0) h+= 360;
-	return {
+	var hsl = {
 		h: h,
 		s: Math.max(0, Math.min(100, parseFloat(match[2]))),
 		l: Math.max(0, Math.min(100, parseFloat(match[3]))),
 		a: 1
 	};
+	if(asPOJO) return hsl;
+	return new Color(hsl);
 };
 
 
-Color.parse.hsla = function parseHSLA(str){
+Color.parse.hsla = function parseHSLA(str, asPOJO){
 	var re = /^\s*hsl\s*\(\s*(\d*\.?\d+)\s*,\s*(\d*\.?\d+)\s*%\s*,\s*(\d*\.?\d+)\s*%\s*,\s*(\d?\.?\d+)\s*\)\s*$/;
 	var match = str.match(re);
 	if(!match) return false;
 	var h = parseFloat(match[1]) % 360;
 	if(h < 0) h+= 360;
-	return {
+	var hsl = {
 		h: h,
 		s: Math.max(0, Math.min(100, parseFloat(match[2]))),
 		l: Math.max(0, Math.min(100, parseFloat(match[3]))),
 		a: Math.max(0, Math.min(1,   parseFloat(match[4])))
 	}
+	if(asPOJO) return hsl;
+	return new Color(hsl);
 };
 
 
